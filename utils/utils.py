233212d480ed0544fe77c21ceac554616f684fa5
tdeng-zuel <3070053397@qq.com>
2021-03-11 12:06:35 +0800

    update

--- utils/utils.py
+++ utils/utils.py
@@ -21,17 +21,14 @@ import os
 
 def get_data(data_name, with_marker=False, norm=False, scale_factor=1e4):
     """
-    description:
-    for specific data_name, get the corresponding data.
+    For specific data_name, get the corresponding data.
 
-    parameters:
-    data_name: the dataset you want to get
-    with_marker: whether to return marker genes
-    norm: whether to normalize features(using the normalization in Seurat)
-    scale_factor: size factor, default 1e4
-
-    return:
-    if with_marker=False, features(row:cell, col:gene, dataframe) and labels(dataframe) of raw data
+    ----------
+    :param data_name: the dataset you want to get
+    :param with_marker: whether to return marker genes
+    :param norm: whether to normalize features(using the normalization in Seurat)
+    :param scale_factor: size factor, default 1e4
+    :return:if with_marker=False, features(row:cell, col:gene, dataframe) and labels(dataframe) of raw data.
     if with_marker=True, features(row:cell, col:gene, dataframe), labels(dataframe) and marker genes(array) of raw data
     """
     if data_name[:4] == 'PBMC':
@@ -81,3 +78,60 @@ def get_data(data_name, with_marker=False, norm=False, scale_factor=1e4):
     else:
         print("parameter 'data_name' is wrong!")
         return None
+
+
+def filter_const_genes(X):
+    """
+    Remove constant genes.
+
+    ----------
+    :param X: Count matrix in  dataframe format (row:cell, col:gene)
+    :return: Filtered count matrix
+    """
+    return X.loc[:, X.mean(axis=0) != 0]
+
+
+def pancreas_filter_and_save_data():
+    """
+    Read separated data, remove cells which do not have a clear cell type and save filtered data.
+
+    ----------
+    :return: None
+    """
+    os.chdir('/home/tdeng/SingleCell/data/pancreas/separated data')
+    not_cell_type = ['unclear', 'not applicable', 'unclassified', 'co-expression', 'beta.contaminated',
+                     'alpha.contaminated', 'delta.contaminated', 'gamma.contaminated']
+
+    for pan_type in ['muraro', 'segerstolpe', 'xin']:
+        # get raw data
+        features = pd.read_csv(pan_type.capitalize() + '_pancreas.csv', index_col=0).T
+        labels = pd.read_csv(pan_type.capitalize() + '_trueID.csv', usecols=[1])
+        # which cells should be kept
+        is_clear = ~labels['x'].isin(not_cell_type).values
+        filtered_features, filtered_labels = features.loc[is_clear, :], labels.loc[is_clear]
+        # save filtered data
+        print('The raw data has {} cells. The filtered data has {} cells.'.format(labels.shape[0],
+                                                                                  filtered_labels.shape[0]))
+        filtered_features.to_csv(pan_type.capitalize() + '_pancreas_filtered.csv')
+        filtered_labels.reset_index(drop=True).to_csv(pan_type.capitalize() + '_trueID_filtered.csv')
+
+
+def get_gene_names(columns):
+    """
+    Get gene names array from features (dataframe).
+    :param columns: dataframe.columns
+    :return: an array contains gene names
+    """
+    if '__' in columns[0]:
+        gene_names = pd.Series(columns).str.split('__', expand=True).iloc[:, 0].values
+    elif '\t' in columns[0]:
+        gene_names = pd.Series(columns).str.split('\t', expand=True).iloc[:, 1].values
+    else:
+        gene_names = pd.Series(columns).values
+    return gene_names
+
+
+X, y = get_data('muraro')
+print(X.shape)
+X = filter_const_genes(X)
+print(X.shape)
