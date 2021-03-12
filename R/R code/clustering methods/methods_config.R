2f2ac50d0bd5a40a421c577371afcc336415a42f
tdeng-zuel <3070053397@qq.com>
2021-03-12 15:59:21 +0800

    delete old version

--- R/R code/clustering methods/methods_config.R
+++
@@ -1,7 +0,0 @@
-
-methods.config.seurat <- list(nfeatures=1000,pc_dims=10,resolution=0.5)
-methods.config.tscan <- list(cvcutoff=0.01,k=8)
-methods.config.sc3 <- list(nfeatures=1000,k=8,gene_filter = T)
-methods.config.sc3.batch_free <- list(nfeatures=1000,k=8,gene_filter = FALSE)
-methods.config.liger <- list(suggestK=F,k.suggest=25,lambda=NULL,resolution=NULL,thresh=NULL)
-
--- R/R code/test.R
+++
@@ -1,4 +0,0 @@
-test <- function(x){
-  print(x)
-  return(as.integer(c(1,2,3)))
-}
