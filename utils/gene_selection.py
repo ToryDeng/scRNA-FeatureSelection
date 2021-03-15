b2f67e62194eb427cfa6d798707a363e58c0338c
tdeng-zuel <3070053397@qq.com>
2021-03-15 14:19:55 +0800

    change a param's name

--- utils/gene_selection.py
+++ utils/gene_selection.py
@@ -5,13 +5,13 @@ import numpy as np
 import os
 
 
-def evaluate_gene_selection_method(dataset=None, methods=None, datatype=None):
+def evaluate_gene_selection_method(dataset=None, methods=None, data_type=None):
     """
     Evaluate certain gene selection methods on specific dataset.
 
     :param dataset: string, the name of dataset
     :param methods: string list, names of gene selection methods
-    :param datatype: string, the method is used on raw or norm data
+    :param data_type: string, the method is used on raw or norm data
     :return: None
     """
     if dataset[:4] == 'PBMC' and 'scGeneFit' in methods:
@@ -22,12 +22,12 @@ def evaluate_gene_selection_method(dataset=None, methods=None, datatype=None):
     print("The dataset has {} cells and {} genes.".format(X_raw.shape[0], X_raw.shape[1]))
     gene_names = get_gene_names(X_raw.columns)
     for method in methods:
-        if datatype == 'raw':
+        if data_type == 'raw':
             result = select_features(dataset, 1000, method, gene_names, X=X_raw.values, y=np.squeeze(y.values))
-        elif datatype == 'norm':
+        elif data_type == 'norm':
             result = select_features(dataset, 1000, method, gene_names, X=X_norm.values, y=np.squeeze(y.values))
         else:
-            print("The parameter 'datatype' is wrong. Please check again.")
+            print("The parameter 'data_type' is wrong. Please check again.")
             return None
         save_filtered_data(X_raw, y, gene_names, result)
         os.system('Rscript scRNA-FeatureSelection/utils/RCode/main.R')
