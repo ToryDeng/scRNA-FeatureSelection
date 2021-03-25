687eae8dea6e0d616480967569d976f20b02d710
tdeng-zuel <3070053397@qq.com>
2021-03-25 18:16:44 +0800

    suppress plot

--- utils/RCode/M3Drop.R
+++ utils/RCode/M3Drop.R
@@ -4,12 +4,13 @@ library("M3Drop")
 
 data_name<-commandArgs(T)
 scex <- get_data(data_name)
-count_matrix = scex@assays@data@listData[["counts"]]
-norm = M3DropConvertData(count_matrix, is.counts=TRUE)
+count_matrix <- scex@assays@data@listData[["counts"]]
+norm <- M3DropConvertData(count_matrix, is.counts=TRUE)
 M3Drop_genes <- M3Drop::M3DropFeatureSelection(
   norm,
   mt_method = "fdr",
-  mt_threshold = 1 # do not filter genes
+  mt_threshold = 1, # do not filter genes
+  suppress.plot = TRUE
 )
 file_name <- paste(data_name,'markers_m3drop', sep = '_')
 write.csv(M3Drop_genes, stringr::str_glue("scRNA-FeatureSelection/tempData/{file_name}.csv"))
